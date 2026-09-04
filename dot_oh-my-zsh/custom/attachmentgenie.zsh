d625d9d5344dedad240c3c7f97fc8c370133cc30
Bram Vogelaar <bram@attachmentgenie.com>
2026-09-05 01:21:09 +0200

    chore: mergning multi platform working copies
    Signed-off-by: Bram Vogelaar <bram@attachmentgenie.com>

--- dot_oh-my-zsh/custom/attachmentgenie.zsh
+++ dot_oh-my-zsh/custom/attachmentgenie.zsh
@@ -1,25 +1,25 @@
-# Put files in this folder to add your own custom functionality.
-# See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
-#
-# Files in the custom/ directory will be:
-# - loaded automatically by the init script, in alphabetical order
-# - loaded last, after all built-ins in the lib/ directory, to override them
-# - ignored by git by default
-#
-# Example: add custom/shortcuts.zsh for shortcuts to your local projects
-#
-# brainstormr=~/Projects/development/planetargon/brainstormr
-# cd $brainstormr
-export GPG_TTY=$(tty)
+export PATH=$PATH:"$HOME"/bin:"$HOME"/DevShed/bin:"$HOME"/go/bin
 
-eval $(thefuck --alias)
-alias f="fuck"
-
-eval "$(rbenv init - zsh)"
+alias apt-yolo='sudo apt update -y;sudo apt dist-upgrade -y;sudo apt autoremove -y'
+alias brew-yolo="brew update && brew upgrade --yes && brew upgrade --cask --greedy --yes"
 
 alias src="cd ~/Devshed/Projects"
 alias sg="cd ~/Devshed/StufisGeneralis"
+alias ssh='cat ~/.ssh/config.d/* >~/.ssh/config; ssh'
+alias guard='bundle exec guard'
+alias kitchen='bundle exec kitchen'
+alias rake='bundle exec rake'
+alias update-git-submodules='git submodule foreach "git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx"'
 
-export PATH=$PATH:/Users/attachmentgenie/bin
+alias tf='terraform fmt'
+alias ti='terraform init'
+alias tv='terraform validate'
+alias tp='terraform plan'
+alias ta='terraform apply -auto-approve'
+alias td='terraform destroy -auto-approve'
+alias tc='rm -rf .terraform *.tfstate *.tfstate.backup'
 
-alias brew-yolo="brew update && brew upgrade --yes && brew upgrade --cask --greedy --yes"
+# proxy settings
+export no_proxy='127.0.0.1,localhost,.vagrant,.intranet,.local,.lab'
+
+neofetch
